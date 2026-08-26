05c413ef136de85f11829fb2db1670a8fa6f4e5f
shirlyk89 <226595573+shirlyk89@users.noreply.github.com>
2026-08-25 20:10:38 -0500

    FIX: ajuste valores estadoOrden.java

--- src/main/java/com/proyectospring/gestionbodega/entities/EstadoOrden.java
+++ src/main/java/com/proyectospring/gestionbodega/entities/EstadoOrden.java
@@ -1,8 +1,8 @@
 package com.proyectospring.gestionbodega.entities;
 
 public enum EstadoOrden {
-    PENDIENTE,
-    EN_PROCESO,
-    COMPLETADA,
-    CANCELADA
+ BORRADOR,
+ APROBADA,
+ RECIBIDA,
+ CANCELADA
 }
